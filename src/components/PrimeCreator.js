a2cb3d5b03b5569e89b02bd20153919c4b98b3f9
AS-Brown <andrewsbrown@hotmail.co.uk>
2022-02-01 12:50:43 +0000

    prime number list created

--- src/components/PrimeCreator.js
+++ src/components/PrimeCreator.js
@@ -1,6 +1,36 @@
-const PrimeCreator = ()=>{
+import React from "react";
+
+const PrimeCreator = ({setPrimeTable}) => {
+
+    const handleLengthChange = (event) => {
+        createPrimeList(event.target.value)
+    }
+
+    const createPrimeList = size => {
+        let currentNum = 2
+        const numArray = []
+        let divideNum = 2
+        while(numArray.length < size){
+            if (currentNum % divideNum === 0 && currentNum !== divideNum){
+                currentNum += 1;
+                divideNum = 2;
+            } else if (currentNum % divideNum === 0 && currentNum === divideNum){
+                numArray.push(currentNum)
+                currentNum += 1 
+                divideNum = 2
+            } else {
+                divideNum += 1;
+            }
+        }
+        setPrimeTable(numArray)
+        }
+
     return (
-        <p>Component</p>
+        <>
+        <form>
+            <input type="number" id="lengthOfGrid" name="lengthOfGrid" required onChange={handleLengthChange} placeholder="Enter grid size"/>
+        </form>
+        </>
     )
 }
 
--- src/containers/ProcessContainer.js
+++ src/containers/ProcessContainer.js
@@ -1,11 +1,14 @@
+import { useState } from "react";
 import PrimeCreator from "../components/PrimeCreator";
 
 const ProcessContainer = ()=>{
+    const [primeTable, setPrimeTable] = useState([]);
+
     return (
-        <div>
-        <p>Container</p>
-        <PrimeCreator />
-        </div>
+    <div>
+        <PrimeCreator setPrimeTable={setPrimeTable}/>
+        <p>The first {primeTable.length} numbers are {primeTable.map(a => a + " ")}</p>
+    </div>
     )
 }
 
